bb441bcc79b0d129b6804152d93e69ac44cbca46
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-04-12 14:45:02 +0100

    ddd

--- 1.0/app.py
+++ 1.0/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 import random
 from non_static.utils import example_util_function, ExampleUtility
-from non_static.quiz import get_random_question, validate_answer
+from non_static.quiz import get_random_question, validate_answer, read_questions
 
 app = Flask(__name__)
+all_questions = read_questions()
+
 
 @app.route('/')
 def index():
@@ -11,7 +13,11 @@ def index():
 
 @app.route('/quiz/question', methods=['GET'])
 def quiz_question():
-    question = get_random_question()
+    question_and_answer = random.choice(all_questions)
+    question = {
+        "question": question_and_answer["question"]
+    }
+    print(f"User asked for a question: {question}")
     return jsonify(question)
 
 @app.route('/quiz/validate', methods=['POST'])
@@ -19,8 +25,16 @@ def quiz_validate():
     # Receive the entire question and the user answer from the request body
     answer = request.json.get('answer')
     question = request.json.get('question')
-    is_correct = validate_answer(question, answer)
+
+    is_correct = False
+    
+    for possible_question in all_questions:
+        if possible_question["question"] == question:
+            is_correct = answer == possible_question["answer"]
+            break
+
+    print(f"User answered question: {question} and said '{answer}'")
     return jsonify({'is_correct': is_correct})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5601)
+    app.run(debug=True, host="0.0.0.0", port=5601)
