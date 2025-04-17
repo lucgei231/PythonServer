db3207f3b067f1e3be301c3da593b93c0b1f10f8
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-04-17 17:48:02 +0100

    modified:   1.0/run.bat

--- 1.0/run.bat
+++ 1.0/run.bat
@@ -1,5 +1,5 @@
 :start
 @echo off
 d:/Desktop/PythonServer/.venv/Scripts/python.exe d:/Desktop/PythonServer/1.0/app.py
-echo There has been an error. Restarting...
+echo There has been an error. Restarting now...
 goto start
